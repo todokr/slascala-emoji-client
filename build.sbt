f8882cc3bcc1daac0a933c8751e14ac46b5155bf
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2017-02-21 22:55:21 +0900

    Change signature

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "slascala-emoji-client"
 
-version := "0.0.1"
+version := "0.0.2"
 
 scalaVersion := "2.12.1"
 
--- src/main/scala/io/github/todokr/SlascalaEmojiClient.scala
+++ src/main/scala/io/github/todokr/SlascalaEmojiClient.scala
@@ -4,36 +4,35 @@ import java.io.File
 import java.nio.charset.StandardCharsets
 import javax.activation.FileTypeMap
 
-import scala.util.control.Exception._
+import scala.util.Try
 
 import org.jsoup.Jsoup
 import skinny.http._
 
 class SlascalaEmojiClient(teamName: String, slackCookie: String) {
 
+
   private val url = s"https://$teamName.slack.com/customize/emoji"
 
-  def upload(emojiName: String, emojiImage: File): Either[Throwable, Unit] = {
+  def upload(emojiName: String, emojiImage: File): Try[Unit] = Try {
 
-    allCatch either {
-      val startPageResponse = HTTP.get(Request(url).header("Cookie", slackCookie))
-      val doc = Jsoup.parse(startPageResponse.textBody)
-      val crumb = doc.body().getElementsByAttributeValue("name", "crumb").attr("value")
-      val mime = FileTypeMap.getDefaultFileTypeMap.getContentType(emojiImage)
-      val multipart = Seq(
-        FormData("add", TextInput("1", StandardCharsets.UTF_8.name)),
-        FormData("crumb", TextInput(crumb, StandardCharsets.UTF_8.name)),
-        FormData("name", TextInput(emojiName, StandardCharsets.UTF_8.name)),
-        FormData("mode", TextInput("data", StandardCharsets.UTF_8.name)),
-        FormData("img", FileInput(emojiImage, mime))
-      )
+    val startPageResponse = HTTP.get(Request(url).header("Cookie", slackCookie))
+    val doc = Jsoup.parse(startPageResponse.textBody)
+    val crumb = doc.body().getElementsByAttributeValue("name", "crumb").attr("value")
+    val mime = FileTypeMap.getDefaultFileTypeMap.getContentType(emojiImage)
+    val multipart = Seq(
+      FormData("add", TextInput("1", StandardCharsets.UTF_8.name)),
+      FormData("crumb", TextInput(crumb, StandardCharsets.UTF_8.name)),
+      FormData("name", TextInput(emojiName, StandardCharsets.UTF_8.name)),
+      FormData("mode", TextInput("data", StandardCharsets.UTF_8.name)),
+      FormData("img", FileInput(emojiImage, mime))
+    )
 
-      val uploadRequest = Request(url).header("Cookie", slackCookie).multipartFormData(multipart)
-      val uploadResponse = HTTP.post(uploadRequest)
+    val uploadRequest = Request(url).header("Cookie", slackCookie).multipartFormData(multipart)
+    val uploadResponse = HTTP.post(uploadRequest)
 
-      val errorMessage = Jsoup.parse(uploadResponse.textBody).select(".alert").not("[style]").text().trim
-      if (errorMessage.nonEmpty) throw new Exception(errorMessage)
-    }
+    val errorMessage = Jsoup.parse(uploadResponse.textBody).select(".alert").not("[style]").text().trim
+    if (errorMessage.nonEmpty) throw new Exception(errorMessage)
   }
 
 }
